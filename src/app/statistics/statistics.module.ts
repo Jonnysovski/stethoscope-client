7a798461b16d1aef683addfbf2559b1b3bc2f4d4
Johnny <yonatald@gmail.com>
2017-03-23 07:47:14 +0200

    Home Statistics
    With fake data

--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -2,20 +2,32 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { MaterialModule } from '@angular/material';
+
+import {BrowserModule} from '@angular/platform-browser'
+import {NgxChartsModule} from '@swimlane/ngx-charts';
 
 import { StatisticsCounterComponent } from './counter/counter.component';
+import { HomeStatisticsComponent } from './home-statistics/home-statistics.component';
+import { BarchartComponent } from './barchart/barchart.component';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
-        SharedModule
+        SharedModule,
+        MaterialModule,
+        BrowserModule, NgxChartsModule 
     ],
     exports: [
-        StatisticsCounterComponent
+        StatisticsCounterComponent,
+        HomeStatisticsComponent,
+        BarchartComponent
     ],
     declarations: [
-        StatisticsCounterComponent
+        StatisticsCounterComponent,
+        HomeStatisticsComponent,
+        BarchartComponent
     ],
     providers: [
 
