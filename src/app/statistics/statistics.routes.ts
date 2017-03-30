ef4c06404e8f96e1bb694105cd32dac98a0c206b
Johnny <yonatald@gmail.com>
2017-03-30 10:01:34 +0300

    statistics
    - Started the foundation of the statistics view

---
+++ src/app/statistics/statistics.routes.ts
@@ -0,0 +1,6 @@
+import { Routes, RouterModule} from '@angular/router';
+import { StatisticsComponent } from './statistics/statistics.component';
+
+export const statisticsRoutes: Routes = [
+    { path: '', component: StatisticsComponent}
+];
---
+++ src/app/statistics/statistics/statistics.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'statistics',
+    templateUrl: 'statistics.component.html',
+    styleUrls: ['statistics.component.scss']
+})
+
+export class StatisticsComponent  { 
+    
+    constructor() {}
+
+
+}
